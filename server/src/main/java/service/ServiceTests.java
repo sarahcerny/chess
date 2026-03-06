f939a1e501a9f84720df369094f3ad4e265c7928
Sarah Cerny <scerny@byu.edu>
2026-03-06 16:30:58 -0700

    working on setup and register success()

--- server/src/main/java/service/ServiceTests.java
+++ server/src/main/java/service/ServiceTests.java
@@ -1,4 +1,26 @@
 package service;
 
+import dataaccess.DataAccessException;
+import dataaccess.MemoryDataAccess;
+import model.AuthData;
+import model.GameData;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class ServiceTests {
+
+    private UserService userService;
+    private GameService gameService;
+
+    @BeforeEach
+    public void setup() throws DataAccessException {
+        var dataAccess = new MemoryDataAccess();
+        userService = new UserService(dataAccess);
+        gameService = new GameService(dataAccess);
+        gameService.clear();
+    }
 }
