91799147484a99abfb980625ed5dc662a318db8e
Sarah Cerny <scerny@byu.edu>
2026-03-06 15:32:18 -0700

    Game service data acess updates depending

--- server/src/main/java/service/GameService.java
+++ server/src/main/java/service/GameService.java
@@ -1,5 +1,62 @@
 package service;
 
-public class GameService {
+import chess.ChessGame;
+import dataaccess.DataAccess;
+import dataaccess.DataAccessException;
+import model.AuthData;
+import model.GameData;
+import java.util.List;
 
+public class GameService {
+    private final DataAccess dataAccess;
+
+    public GameService(DataAccess dataAccess) {
+        this.dataAccess = dataAccess;
+    }
+
+    public void clear() throws DataAccessException {
+        dataAccess.clear();
+    }
+
+    public List<GameData> listGames(String authToken) throws DataAccessException {
+        if (dataAccess.getAuth(authToken) == null) {
+            throw new DataAccessException("Error: unauthorized");
+        }
+        return dataAccess.listGames();
+    }
+
+    public int createGame(String authToken, String gameName) throws DataAccessException {
+        if (dataAccess.getAuth(authToken) == null) {
+            throw new DataAccessException("Error: unauthorized");
+        }
+        if (gameName == null) {
+            throw new DataAccessException("Error: bad request");
+        }
+        GameData game = new GameData(0, null, null, gameName, new ChessGame());
+        return dataAccess.createGame(game);
+    }
+
+    public void joinGame(String authToken, String playerColor, int gameID) throws DataAccessException {
+        AuthData auth = dataAccess.getAuth(authToken);
+        if (auth == null) {
+            throw new DataAccessException("Error: unauthorized");
+        }
+        GameData game = dataAccess.getGame(gameID);
+        if (game == null || playerColor == null) {
+            throw new DataAccessException("Error: bad request");
+        }
+        if (playerColor.equals("WHITE")) {
+            if (game.whiteUsername() != null) {
+                throw new DataAccessException("Error: already taken");
+            }
+            dataAccess.updateGame(new GameData(game.gameID(), auth.username(), game.blackUsername(), game.gameName(), game.game()));
+        } else if (playerColor.equals("BLACK")) {
+            if (game.blackUsername() != null) {
+                throw new DataAccessException("Error: already taken");
+            }
+            dataAccess.updateGame(new GameData(game.gameID(), game.whiteUsername(), auth.username(), game.gameName(), game.game()));
+        } else {
+            throw new DataAccessException("Error: bad request");
+        }
+    }
 }
