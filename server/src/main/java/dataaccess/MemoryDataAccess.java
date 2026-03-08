1ebe0da49c81369d472eba6eb8c1615d26809c2f
Sarah Cerny <scerny@byu.edu>
2026-03-07 17:43:49 -0700

    MemoryDataAccess.java

--- server/src/main/java/dataaccess/MemoryDataAccess.java
+++ server/src/main/java/dataaccess/MemoryDataAccess.java
@@ -9,52 +9,54 @@ import java.util.List;
 import java.util.Map;
 
 public class MemoryDataAccess implements DataAccess {
-    private final Map<String, UserData> users = new HashMap<>();
-    private final Map<Integer, GameData> games = new HashMap<>();
-    private final Map<String, AuthData> auths = new HashMap<>();
-    private int nextGameID = 1;
+    private final Map<String, UserData> realPlayers = new HashMap<>();
+    private final Map<Integer, GameData> activeGames = new HashMap<>();
+    private final Map<String, AuthData> activePlayers = new HashMap<>();
+    private int futureGameID = 1;
 
     public void clear() {
-        users.clear();
-        games.clear();
-        auths.clear();
+        realPlayers.clear();
+        activeGames.clear();
+        activePlayers.clear();
+        futureGameID = 1;
     }
 
     public void createUser(UserData user) throws DataAccessException {
-        users.put(user.username(), user);
+        realPlayers.put(user.username(), user);
     }
 
     public UserData getUser(String username) throws DataAccessException {
-        return users.get(username);
+        return realPlayers.get(username);
     }
 
     public int createGame(GameData game) throws DataAccessException {
-        int id = nextGameID++;
-        games.put(id, new GameData(id, game.whiteUsername(), game.blackUsername(), game.gameName(), game.game()));
+        int id = futureGameID++;
+        GameData withID = new GameData(id, game.whiteUsername(), game.blackUsername(), game.gameName(), game.game());
+        activeGames.put(id, withID);
         return id;
     }
 
     public GameData getGame(int gameID) throws DataAccessException {
-        return games.get(gameID);
+        return activeGames.get(gameID);
     }
 
     public List<GameData> listGames() throws DataAccessException {
-        return new ArrayList<>(games.values());
+        return new ArrayList<>(activeGames.values());
     }
 
     public void updateGame(GameData game) throws DataAccessException {
-        games.put(game.gameID(), game);
+        activeGames.put(game.gameID(), game);
     }
 
     public void createAuth(AuthData auth) throws DataAccessException {
-        auths.put(auth.authToken(), auth);
+        activePlayers.put(auth.authToken(), auth);
     }
 
     public AuthData getAuth(String authToken) throws DataAccessException {
-        return auths.get(authToken);
+        return activePlayers.get(authToken);
     }
 
     public void deleteAuth(String authToken) throws DataAccessException {
-        auths.remove(authToken);
+        activePlayers.remove(authToken);
     }
 }
