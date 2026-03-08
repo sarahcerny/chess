8bb48a4de00591546ba076424a1fee6d3553f2d4
Sarah Cerny <scerny@byu.edu>
2026-03-07 19:02:18 -0700

    GameService.java going through it and writing out whats going on.

--- server/src/main/java/service/GameService.java
+++ server/src/main/java/service/GameService.java
@@ -9,53 +9,72 @@ import model.GameData;
 import java.util.List;
 
 public class GameService {
+
+    // dataAccess does all the heavy lifting of actually storing stuff
     private final DataAccess dataAccess;
 
     public GameService(DataAccess dataAccess) {
         this.dataAccess = dataAccess;
     }
 
+    // wipes literally everything bye bye data you were peak once
     public void clear() throws DataAccessException {
         dataAccess.clear();
     }
 
+
+    // gotta check the token first before showing any games
     public List<GameData> listGames(String playerToken) throws DataAccessException {
+
+        // if we dont recognize the token they are NOT getting the list lol
         if (dataAccess.getAuth(playerToken) == null) {
             throw new DataAccessException("Error: unauthorized");
         }
         return dataAccess.listGames();
     }
+    // make a brand new chess game and store it in your bag
     public int createGame(String playerToken, String gameTitle) throws DataAccessException {
         if (dataAccess.getAuth(playerToken) == null) {
             throw new DataAccessException("Error: unauthorized");
         }
+
+        // also need an actual name for the game
+
         if (gameTitle == null) {
             throw new DataAccessException("Error: bad request");
         }
+        // 0 is placeholder id temporarily carrying the team
         GameData newGameBoard = new GameData(0, null, null, gameTitle, new ChessGame());
         return dataAccess.createGame(newGameBoard);
     }
 
+    // player picking their side and jumping in go to war
     public void joinGame(String playerToken, String teamColor, int chessGameID) throws DataAccessException {
         AuthData playerSession = dataAccess.getAuth(playerToken);
+        //who are you
         if (playerSession == null) {
             throw new DataAccessException("Error: unauthorized");
         }
+        //are you real
         GameData currentGame = dataAccess.getGame(chessGameID);
         if (currentGame == null || teamColor == null) {
             throw new DataAccessException("Error: bad request");
         }
+        //white spot taken finders keepers
         if (teamColor.equals("WHITE")) {
             if (currentGame.whiteUsername() != null) {
                 throw new DataAccessException("Error: already taken");
             }
             dataAccess.updateGame(new GameData(currentGame.gameID(), playerSession.username(), currentGame.blackUsername(), currentGame.gameName(), currentGame.game()));
         } else if (teamColor.equals("BLACK")) {
+            //black spot taken finders keepers
             if (currentGame.blackUsername() != null) {
                 throw new DataAccessException("Error: already taken");
             }
+            //locking in black lets win this
             dataAccess.updateGame(new GameData(currentGame.gameID(), currentGame.whiteUsername(), playerSession.username(), currentGame.gameName(), currentGame.game()));
         } else {
+            // teamColor has to be WHITE or BLACK mj song again
             throw new DataAccessException("Error: bad request");
         }
     }
