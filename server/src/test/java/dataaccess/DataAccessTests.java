d48d2002ac387bed447c1637faee112ec3d0677e
Sarah Cerny <scerny@byu.edu>
2026-03-11 15:53:40 -0600

    DataAccessTests.java fresh start wipe it clean before every test

--- server/src/test/java/dataaccess/DataAccessTests.java
+++ server/src/test/java/dataaccess/DataAccessTests.java
@@ -1,4 +1,36 @@
 package dataaccess;
 
-public class DataAccessTests {
+import chess.ChessGame;
+import model.GameData;
+import model.AuthData;
+import model.UserData;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import service.UserService;
+// import service.AuthService;
+import service.GameService;
+import javax.xml.crypto.Data;
+
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class DAOTests {
+
+    private SqlUserDAO userDAO;
+    private SqlAuthDAO authDAO;
+    private SqlGameDAO gameDAO;
+
+    @BeforeEach
+    public void freshStart() throws DataAccessException {
+        userDAO = new SqlUserDAO();
+        authDAO = new SqlAuthDAO();
+        gameDAO = new SqlGameDAO();
+        // wipe it all clean before every test no leftovers
+        userDAO.clear();
+        authDAO.clear();
+        gameDAO.clear();
+    }
+
 }
