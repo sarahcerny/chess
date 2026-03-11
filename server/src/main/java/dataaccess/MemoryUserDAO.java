ce49093713867c275e5478b25c163891f43889aa
Sarah Cerny <scerny@byu.edu>
2026-03-11 13:05:40 -0600

    Working on MemoryUserDAO.java. and creating a draft for sqlUser.java

--- server/src/main/java/dataaccess/MemoryUserDAO.java
+++ server/src/main/java/dataaccess/MemoryUserDAO.java
@@ -1,4 +1,21 @@
 package dataaccess;
 
-public class MemoryUserDAO {
+import model.UserData;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryUserDAO implements UserDAO {
+    private final Map<String, UserData> users = new HashMap<>();
+
+    public void clear() {
+        users.clear();
+    }
+
+    public void createUser(UserData user) throws DataAccessException {
+        users.put(user.username(), user);
+    }
+
+    public UserData getUser(String username) throws DataAccessException {
+        return users.get(username);
+    }
 }
--- server/src/main/java/dataaccess/SqlUserDAO.java
+++ server/src/main/java/dataaccess/SqlUserDAO.java
@@ -1,4 +1,43 @@
 package dataaccess;
 
-public class SqlUserDAO {
+import model.UserData;
+import org.mindrot.jbcrypt.BCrypt;
+import java.sql.*;
+
+public class SqlUserDAO implements UserDAO {
+
+    public void clear() throws DataAccessException {
+        try (var conn = DatabaseManager.getConnection();
+             var ps = conn.prepareStatement("TRUNCATE TABLE users")) {
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw new DataAccessException("Unable to clear users", e);
+        }
+    }
+
+    public void createUser(UserData user) throws DataAccessException {
+        try (var conn = DatabaseManager.getConnection();
+             var ps = conn.prepareStatement("INSERT INTO users (username, password, email) VALUES (?, ?, ?)")) {
+            ps.setString(1, user.username());
+            ps.setString(2, BCrypt.hashpw(user.password(), BCrypt.gensalt()));
+            ps.setString(3, user.email());
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw new DataAccessException("Unable to create user", e);
+        }
+    }
+
+    public UserData getUser(String username) throws DataAccessException {
+        try (var conn = DatabaseManager.getConnection();
+             var ps = conn.prepareStatement("SELECT username, password, email FROM users WHERE username=?")) {
+            ps.setString(1, username);
+            var rs = ps.executeQuery();
+            if (rs.next()) {
+                return new UserData(rs.getString("username"), rs.getString("password"), rs.getString("email"));
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException("Unable to get user", e);
+        }
+        return null;
+    }
 }
