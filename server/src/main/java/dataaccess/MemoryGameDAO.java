d4ac42b33c71896a624834fc39bacf5cf36b80ff
Sarah Cerny <scerny@byu.edu>
2026-03-11 12:49:06 -0600

    Working on MemoryGameDAO.java.

--- server/src/main/java/dataaccess/MemoryGameDAO.java
+++ server/src/main/java/dataaccess/MemoryGameDAO.java
@@ -1,4 +1,36 @@
 package dataaccess;
 
-public class MemoryGameDAO {
+import model.GameData;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MemoryGameDAO implements GameDAO {
+    private final Map<Integer, GameData> games = new HashMap<>();
+    private int nextID = 1;
+
+    public void clear() {
+        games.clear();
+        nextID = 1;
+    }
+
+    public int createGame(GameData game) throws DataAccessException {
+        int id = nextID++;
+        GameData withID = new GameData(id, game.whiteUsername(), game.blackUsername(), game.gameName(), game.game());
+        games.put(id, withID);
+        return id;
+    }
+
+    public GameData getGame(int gameID) throws DataAccessException {
+        return games.get(gameID);
+    }
+
+    public List<GameData> listGames() throws DataAccessException {
+        return new ArrayList<>(games.values());
+    }
+
+    public void updateGame(GameData game) throws DataAccessException {
+        games.put(game.gameID(), game);
+    }
 }
