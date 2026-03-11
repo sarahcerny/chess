bebd7dd0eb40fefe8e701dcc4d745b485c92d9f5
Sarah Cerny <scerny@byu.edu>
2026-03-11 12:13:38 -0600

    going back into service to add my new titles and folders in correctly initially I did it incorrect.

--- server/src/main/java/dataaccess/GameDAO.java
+++ server/src/main/java/dataaccess/GameDAO.java
@@ -1,4 +1,12 @@
 package dataaccess;
 
+import model.GameData;
+import java.util.List;
+
 public interface GameDAO {
+    void clear() throws DataAccessException;
+    int createGame(GameData game) throws DataAccessException;
+    GameData getGame(int gameID) throws DataAccessException;
+    List<GameData> listGames() throws DataAccessException;
+    void updateGame(GameData game) throws DataAccessException;
 }
--- server/src/main/java/dataaccess/UserDAO.java
+++ server/src/main/java/dataaccess/UserDAO.java
@@ -1,4 +1,9 @@
 package dataaccess;
 
+import model.UserData;
+
 public interface UserDAO {
+    void clear() throws DataAccessException;
+    void createUser(UserData user) throws DataAccessException;
+    UserData getUser(String username) throws DataAccessException;
 }
