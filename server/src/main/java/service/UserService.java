1b759d9a8a8f1f143cf8b0a8371ecd9d75e5e793
Sarah Cerny <scerny@byu.edu>
2026-03-11 12:18:51 -0600

    Mega updating my game service to take in DAO folder instead of what I had before which was wrong.

--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -1,7 +1,8 @@
 package service;
 
-import dataaccess.DataAccess;
+import dataaccess.AuthDAO;
 import dataaccess.DataAccessException;
+import dataaccess.UserDAO;
 import model.AuthData;
 import model.UserData;
 
@@ -10,11 +11,12 @@ import java.util.UUID;
 // handles everything player account related
 public class UserService {
 
-    // dataAccess is where all the actual data lives
-    private final DataAccess dataAccess;
+    private final UserDAO userDAO;
+    private final AuthDAO authDAO;
 
     public UserService(UserDAO userDAO, AuthDAO authDAO) {
-        this.dataAccess = dataAccess;
+        this.userDAO = userDAO;
+        this.authDAO = authDAO;
     }
 
     // brand new player wants in deal them
@@ -23,40 +25,40 @@ public class UserService {
             throw new DataAccessException("Error: bad request");
         }
         // already got someone with that name no identity fraud here sis
-        if (dataAccess.getUser(playerName) != null) {
+        if (userDAO.getUser(playerName) != null) {
             throw new DataAccessException("Error: already taken");
         }
         // all good fresh token for them game time yay
-        dataAccess.createUser(new UserData(playerName, password, playerEmail));
+        userDAO.createUser(new UserData(playerName, password, playerEmail));
         AuthData playerSession = new AuthData(UUID.randomUUID().toString(), playerName);
-        dataAccess.createAuth(playerSession);
+        authDAO.createAuth(playerSession);
         return playerSession;
     }
 
-    //check their password and hook them up with a new token
+    // check their password and hook them up with a new token
     public AuthData login(String playerName, String password) throws DataAccessException {
         // missing info bad request no cap
         if (playerName == null || password == null) {
             throw new DataAccessException("Error: bad request");
         }
         // look them up in the system aka stalk
-        UserData playerData = dataAccess.getUser(playerName);
+        UserData playerData = userDAO.getUser(playerName);
         // either they dont exist or wrong password
         if (playerData == null || !playerData.password().equals(password)) {
             throw new DataAccessException("Error: unauthorized");
         }
         // yay they passed
         AuthData playerSession = new AuthData(UUID.randomUUID().toString(), playerName);
-        dataAccess.createAuth(playerSession);
+        authDAO.createAuth(playerSession);
         return playerSession;
     }
 
     // player is done for now
     public void logout(String playerToken) throws DataAccessException {
-        if (dataAccess.getAuth(playerToken) == null) {
+        if (authDAO.getAuth(playerToken) == null) {
             throw new DataAccessException("Error: unauthorized");
         }
         // token goes bye bye
-        dataAccess.deleteAuth(playerToken);
+        authDAO.deleteAuth(playerToken);
     }
 }
