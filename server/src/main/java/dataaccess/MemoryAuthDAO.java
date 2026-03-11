f6ecedce1c6e94b83469852164acf613d82f6adf
Sarah Cerny <scerny@byu.edu>
2026-03-11 12:54:31 -0600

    Working on MemoryAuthDAO.java

--- server/src/main/java/dataaccess/MemoryAuthDAO.java
+++ server/src/main/java/dataaccess/MemoryAuthDAO.java
@@ -1,4 +1,25 @@
 package dataaccess;
 
-public class MemoryAuthDAO {
+import model.AuthData;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryAuthDAO implements AuthDAO {
+    private final Map<String, AuthData> auths = new HashMap<>();
+
+    public void clear() {
+        auths.clear();
+    }
+
+    public void createAuth(AuthData auth) throws DataAccessException {
+        auths.put(auth.authToken(), auth);
+    }
+
+    public AuthData getAuth(String authToken) throws DataAccessException {
+        return auths.get(authToken);
+    }
+
+    public void deleteAuth(String authToken) throws DataAccessException {
+        auths.remove(authToken);
+    }
 }
