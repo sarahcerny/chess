b0a111aa7d43d4b95f4683f079ed3958e702c27e
Sarah Cerny <scerny@byu.edu>
2026-03-21 17:49:43 -0600

    working on run

--- client/src/test/java/client/Repl.java
+++ client/src/test/java/client/Repl.java
@@ -1,4 +1,20 @@
 package client;
 
+import java.util.Scanner;
+
 public class Repl {
+    public Repl(String serverUrl) {
+    }
+
+    public void run() {
+        System.out.println("♕ Welcome to 240 chess. Type Help to get started. ♕");
+        Scanner scanner = new Scanner(System.in);
+        String line = "";
+        while (!line.equalsIgnoreCase("quit")) {
+            System.out.print("[LOGGED_OUT] >>> ");
+            line = scanner.nextLine().trim();
+            System.out.println(eval(line));
+        }
+    }
+
 }
