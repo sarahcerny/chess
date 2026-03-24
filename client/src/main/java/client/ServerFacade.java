88d0b766c836eab70afe191dbce3a9428e77a169
Sarah Cerny <scerny@byu.edu>
2026-03-23 21:14:18 -0600

    creating final strings for serverfacade

--- client/src/main/java/client/ServerFacade.java
+++ client/src/main/java/client/ServerFacade.java
@@ -1,4 +1,22 @@
 package client;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.*;
+import com.google.gson.Gson;
+
 public class ServerFacade {
-}
+
+    private final String serverAddress;
+    private String sessionToken;
+    private final HttpClient netClient;
+    private String currentUser;
+    private final Gson gson;
+
+    public ServerFacade(int port) {
+        this.serverAddress = "http://localhost:" + port;
+        this.netClient = HttpClient.newHttpClient();
+        this.gson = new Gson();
+    }
