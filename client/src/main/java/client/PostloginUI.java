8af5a722213f5ae2f2941c34ee42e92f2aec8cf7
Sarah Cerny <scerny@byu.edu>
2026-03-23 22:40:53 -0600

    run with setting

--- client/src/main/java/client/PostloginUI.java
+++ client/src/main/java/client/PostloginUI.java
@@ -21,4 +21,35 @@ public class PostloginUI {
         this.input = input;
     }
 
+    public void run() {
+        System.out.println(SET_TEXT_COLOR_YELLOW + "Logged in! Type 'help' to see your options.");
+        while (true) {
+            System.out.print(SET_TEXT_COLOR_WHITE + "[LOGGED_IN] >>> ");
+            String userAction = input.nextLine().trim();
+            var inputParts = userAction.toLowerCase().split(" ");
+            var holdInput = (inputParts.length > 0) ? inputParts[0] : "help";
+            var args = Arrays.copyOfRange(inputParts, 1, inputParts.length);
+
+            String result = switch (holdInput) {
+                case "help"    -> helpMenu();
+                case "logout"  -> {
+                    logout();
+                    yield "logout";
+                }
+                case "create"  -> makeGame(args);
+                case "list"    -> getGames();
+                case "play"    -> playChess(args);
+                case "observe" -> viewGame(args);
+                case "quit"    -> {
+                    System.out.println(SET_TEXT_COLOR_RED + "Leaving chess. Goodbye!");
+                    yield "quit";
+                }
+                default -> SET_TEXT_COLOR_RED + "Unknown command. Type 'help' to see available commands.";
+            };
+
+            System.out.println(result);
+            if (result.equals("logout") || result.equals("quit")) return;
+        }
+    }
+
 }
