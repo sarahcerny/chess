db78c6f8bc1017a45c3f2713ccd0065c640699bd
Sarah Cerny <scerny@byu.edu>
2026-03-24 09:32:29 -0600

    color the board

--- client/src/main/java/client/ChessBoardPrinter.java
+++ client/src/main/java/client/ChessBoardPrinter.java
@@ -6,4 +6,25 @@ import static ui.EscapeSequences.*;
 public class ChessBoardPrinter {
 
     private static final String EMPTY = "   ";
+    public static void drawBoard(ChessGame game, String teamColor) {
+        ChessBoard board = game.getBoard();
+        boolean isWhite = teamColor.equalsIgnoreCase("WHITE");
+        System.out.println();
+        printColumnLabels(isWhite);
+        for (int row = 0; row < 8; row++) {
+            int actualRow = isWhite ? (8 - row) : (row + 1);
+            System.out.print(SET_TEXT_COLOR_WHITE + " " + actualRow + " ");
+            for (int col = 0; col < 8; col++) {
+                int actualCol = isWhite ? (col + 1) : (8 - col);
+                boolean isLightSquare = (actualRow + actualCol) % 2 == 0;
+                String bgColor = isLightSquare ? SET_BG_COLOR_WHITE : SET_BG_COLOR_BLACK;
+                ChessPosition pos = new ChessPosition(actualRow, actualCol);
+                ChessPiece piece = board.getPiece(pos);
+                System.out.print(bgColor + getPieceDisplay(piece));
+            }
+            System.out.print(RESET_BG_COLOR + SET_TEXT_COLOR_WHITE + " " + actualRow + " ");
+            System.out.println();
+        }
+    }
+
 
