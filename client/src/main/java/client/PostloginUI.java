4ba57cbfde7eb83c2d268be76eac8bc744ed9217
Sarah Cerny <scerny@byu.edu>
2026-03-23 22:33:04 -0600

    start PostloginUI.java

--- client/src/main/java/client/PostloginUI.java
+++ client/src/main/java/client/PostloginUI.java
@@ -1,4 +1,24 @@
 package client;
 
+import chess.ChessGame;
+import model.GameData;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import static ui.EscapeSequences.*;
+
 public class PostloginUI {
+
+    private final ServerFacade serverFacade;
+    private final Scanner input;
+    private ArrayList<GameData> gamesList;
+    private final Map<Integer, GameData> gameMap = new HashMap<>();
+
+    public PostloginUI(ServerFacade serverFacade, Scanner input) {
+        this.serverFacade = serverFacade;
+        this.input = input;
+    }
+
 }
