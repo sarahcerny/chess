2698f85beb35edde3c75a07d8a91a48caf13ba16
Sarah Cerny <scerny@byu.edu>
2026-03-23 22:12:40 -0600

    now lets start Prelogin UI

--- client/src/main/java/client/PreloginUI.java
+++ client/src/main/java/client/PreloginUI.java
@@ -1,4 +1,21 @@
 package client;
 
+import java.util.Arrays;
+import java.util.Scanner;
+import static ui.EscapeSequences.*;
+
 public class PreloginUI {
+    private final ServerFacade serverFacade;
+    private final String serverAddress;
+    private String sessionToken;
+    private final Scanner input;
+
+    public PreloginUI(ServerFacade serverFacade) {
+        this.serverFacade = serverFacade;
+        this.serverAddress = serverFacade.getSessionToken();
+        this.sessionToken = "";
+        this.input = new Scanner(System.in);
+    }
 }
+
+
