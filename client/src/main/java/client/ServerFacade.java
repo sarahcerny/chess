a5650d16dbebeb77666a53761ceda37cba33a85a
Sarah Cerny <scerny@byu.edu>
2026-03-23 21:32:50 -0600

    login and register and log out

--- client/src/main/java/client/ServerFacade.java
+++ client/src/main/java/client/ServerFacade.java
@@ -16,3 +16,29 @@ public class ServerFacade {
     public ServerFacade(int port) {
         serverAddress = "http://localhost:" + port;
     }
+    // gotta authenticate that shit.
+    public String login(String username, String password) {
+        AuthData received = callServer("POST", "/session",
+                new UsernameAndPassword(username, password), AuthData.class);
+        sessionToken = received.authToken();
+        return sessionToken;
+    }
+
+    public String register(String username, String password, String email) {
+        AuthData received = callServer("POST", "/user",
+                new UserData(username, password, email), AuthData.class);
+        sessionToken = received.authToken();
+        return sessionToken;
+    }
+
+    public void logout() {
+        requireLogin();
+        callServer("DELETE", "/session", null, null);
+        sessionToken = null;
+    }
+    // let the games begin
+
+
+
+
+}
