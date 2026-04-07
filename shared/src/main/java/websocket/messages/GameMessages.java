95588ae71482ee14e8d3460653bac6a98ea50fa6
Sarah Cerny <scerny@byu.edu>
2026-04-07 09:44:21 -0600

    GameMessages

--- shared/src/main/java/websocket/messages/GameMessages.java
+++ shared/src/main/java/websocket/messages/GameMessages.java
@@ -1,4 +1,16 @@
 package websocket.messages;
 
-public class GameMessages {
+import chess.ChessGame;
+
+public class GameMessages extends ServerMessage {
+    private final ChessGame gameState;
+
+    public GameMessages(ChessGame gameState) {
+        super(ServerMessageType.LOAD_GAME);
+        this.gameState = gameState;
+    }
+
+    public ChessGame getGameState() {
+        return gameState;
+    }
 }
