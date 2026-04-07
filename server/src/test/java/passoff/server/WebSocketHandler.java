07dd0ef389ddb8c90f16f975cbd3dac05ce90fe9
Sarah Cerny <scerny@byu.edu>
2026-04-07 12:33:05 -0600

    WebSocketHandler.java

--- server/src/test/java/passoff/server/WebSocketHandler.java
+++ server/src/test/java/passoff/server/WebSocketHandler.java
@@ -1,4 +1,29 @@
-package passoff.server;
+package server;
+
+import com.google.gson.Gson;
+import dataaccess.AuthDAO;
+import dataaccess.GameDAO;
+import io.javalin.websocket.WsContext;
+
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class WebSocketHandler {
+
+    private final AuthDAO authDAO;
+    private final GameDAO gameDAO;
+    private final Gson gson = new Gson();
+
+    // tracks all sessions by session key and id
+    private final Map<String, WsContext> playerSessions = new ConcurrentHashMap<>();
+    // tracks which sessions belong to which game room
+    private final Map<Integer, Set<String>> roomSessions = new ConcurrentHashMap<>();
+
+    public WebSocketHandler(AuthDAO authDAO, GameDAO gameDAO) {
+        this.authDAO = authDAO;
+        this.gameDAO = gameDAO;
+    }
+
+
 }
