39be2cd722e65bd0d1eedc69ae030c101416f00b
Sarah Cerny <scerny@byu.edu>
2026-04-07 09:55:38 -0600

    moveCommands

--- shared/src/main/java/websocket/commands/MoveCommands.java
+++ shared/src/main/java/websocket/commands/MoveCommands.java
@@ -1,4 +1,16 @@
 package websocket.commands;
 
-public class MoveCommands {
+import chess.ChessMove;
+
+public class MoveCommands extends UserGameCommand {
+    private final ChessMove playerMove;
+
+    public MoveCommands(String authToken, Integer gameID, ChessMove playerMove) {
+        super(CommandType.MAKE_MOVE, authToken, gameID);
+        this.playerMove = playerMove;
+    }
+
+    public ChessMove getPlayerMove() {
+        return playerMove;
+    }
 }
