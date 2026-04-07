4fd36944ea989b4db709ffa93322e21690972754
Sarah Cerny <scerny@byu.edu>
2026-04-07 12:37:59 -0600

    on connect on close

--- server/src/test/java/passoff/server/WebSocketHandler.java
+++ server/src/test/java/passoff/server/WebSocketHandler.java
@@ -1,9 +1,18 @@
 package server;
 
+import chess.ChessGame;
+import chess.ChessMove;
 import com.google.gson.Gson;
-import dataaccess.AuthDAO;
-import dataaccess.GameDAO;
+import dataaccess.*;
 import io.javalin.websocket.WsContext;
+import io.javalin.websocket.WsMessageContext;
+import model.AuthData;
+import model.GameData;
+import websocket.commands.MoveCommands;
+import websocket.commands.UserGameCommand;
+import websocket.messages.ErrorMessage;
+import websocket.messages.GameMessages;
+import websocket.messages.NotificationMessage;
 
 import java.util.Map;
 import java.util.Set;
@@ -23,6 +32,14 @@ public class WebSocketHandler {
     public WebSocketHandler(AuthDAO authDAO, GameDAO gameDAO) {
         this.authDAO = authDAO;
         this.gameDAO = gameDAO;
+    }
+    public void onConnect(WsContext session) {
+        playerSessions.put(session.getSessionId(), session);
+    }
+
+    public void onClose(WsContext session) {
+        playerSessions.remove(session.getSessionId());
+        roomSessions.values().forEach(set -> set.remove(session.getSessionId()));
     }
 
 
